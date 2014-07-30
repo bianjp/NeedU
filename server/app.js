6b1d9f795d15a4889f6961ca79f8a52978415203
边家平 <ssbianjp@gmail.com>
2014-07-30 11:20:39 +0800

    修改错误处理; 增加错误页面模板

--- server/app.js
+++ server/app.js
@@ -22,34 +22,6 @@ app.use(multer({ dest: './public/temp/'}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-
-  app.get('/test', function(req, res){
-    res.render('test');
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
 var db = require('./lib/db');
 db.getConnection(function(db) {
   app.use(function(req, res, next){
@@ -64,20 +36,43 @@ db.getConnection(function(db) {
   app.use('/admin', require('./routes/admin'));
   app.use('/api', require('./routes/api'));
 
+  // Test page
+  if (app.get('env') === 'development') {
+    app.get('/test', function(req, res){
+      res.render('test');
+    });
+  }
+
   /// catch 404 and forward to error handler
   app.use(function(req, res, next) {
-    if (req.path.indexOf('api') != -1){
-      res.send({
-        status: 404,
-        message: 'Not Founded'
-      });
-    }
-    else{
-      var err = new Error('Not Found');
-      err.status = 404;
-      next(err);
-    }
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
   });
+
+  /// error handlers
+
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err
+      });
+    });
+  }
+
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: {}
+    });
+});
 
   var server = app.listen(process.env.PORT || 3000, function() {
     console.log('Express server listening on port ' + server.address().port);
